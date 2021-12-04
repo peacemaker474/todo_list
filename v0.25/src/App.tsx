4e5b6e7714992a2bcd4d5d357865d51676e0b59d
{hc456123@gmail.com} <hc456123@gmail.com>
2021-12-04 21:18:54 +0900

    2021-12-04 Add Recoil Todo List

---
+++ v0.25/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+import ToDoList from './ToDoList';
+
+
+function App () {
+  return (
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
+  )
+}
+
+export default App;
